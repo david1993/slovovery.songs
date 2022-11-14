f0561d8783c44cbe09cbc22e583ec971716f147e
David.Polishhuk <david.polishhuk@tatar.ru>
2022-11-14 16:13:14 +0300

    +

--- application/classes/controller/song.php
+++ application/classes/controller/song.php
@@ -141,7 +141,7 @@ class Controller_Song extends Controller_Layout
         if ($song->loaded() and ($song->published() or Auth::instance()->logged_in('admin'))) {
             $newId = (int)json_decode(file_get_contents('http://slovovery.ru/24_7/song/redirect_to/?id=' . $song->id), true);
             $newSongUrl = 'http://slovovery.ru/24_7/song/' . $newId . '/';
-            if (strpos($_SERVER['HTTP_REFERER'], 'http://slovovery.ru/') === 0) {
+            if (isset($_SERVER['HTTP_REFERER']) && strpos($_SERVER['HTTP_REFERER'], 'http://slovovery.ru/') === 0) {
                 header('Location: ' . $newSongUrl);
                 exit;
             }
